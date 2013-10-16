41f796a74c98c8fc1fe476dc1ec9bf918ebc39a7
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-10-16 10:59:39 +0300

    Initial commit

---
+++ languages/en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English languages file
+ */
+
+$lang = array(
+	'personal_activity' => 'Activity',
+	'personal_activity:owner' => 'Activity of %s',
+	'personal_activity:owner:self' => 'Your own activity', 
+);
+
+add_translation('en', $lang);
